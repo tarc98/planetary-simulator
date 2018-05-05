cfbd899f12eaa02128e2b2b57317e6885cf94965
Tomasz Arczewski <t.arczewski@wp.pl>
2018-05-05 03:16:03 +0200

    Code clarity

--- planetary-simulator/src/main/simulator/planetary/Main.java
+++ planetary-simulator/src/main/simulator/planetary/Main.java
@@ -16,6 +16,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
@@ -50,118 +51,35 @@ public class Main extends Application {
 
     public static Stage mainStage;
 
+    public static PlanetaryMenuBar menuBar;
+    public static HBox rightBox;
+    public static VBox bottomBox;
+    public static Pane mainBox;
+
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle(TITLE+" v. "+VERSION);
         mainStage=stage;
+        stage.setTitle(TITLE+" v. "+VERSION);
 
         BorderPane layout=new BorderPane();
         Scene scene=new Scene(layout, width, height);
 
-        EventHandler<ActionEvent> e=new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                mainStage.close();
-            }
-        };
+        menuBar=PlanetaryGUI.makePlanetaryBar();
+        rightBox=PlanetaryGUI.makeRightBox();
+        bottomBox=PlanetaryGUI.makeBottomBox();
+        mainBox=PlanetaryGUI.makeMainBox();
 
-        PlanetaryMenuBar menuBar=new PlanetaryMenuBar();
-        menuBar.addMenu("File");
-        menuBar.addMenuItem("File", "Save...", null);
-        menuBar.addMenuItem("File", "Save as...", null);
-        menuBar.addSeparator("File");
-        menuBar.addMenuItem("File", "Exit", BasicEvents.closeEvent);
-        menuBar.addMenu("Edit");
-        menuBar.addMenuItem("Edit", "Undo", null);
-        menuBar.addMenuItem("Edit", "Redo", null);
-        menuBar.addSeparator("Edit");
-        menuBar.addMenuItem("Edit", "Cut", null);
-        menuBar.addMenuItem("Edit", "Copy", null);
-        menuBar.addMenuItem("Edit", "Paste", null);
-        menuBar.addMenu("Help");
-        menuBar.addMenuItem("Help", "About", null);
-
-        Pane center=new Pane();
-        HBox right=new HBox();
-        right.setMinWidth(250);
-        Separator sep1=new Separator(Orientation.VERTICAL);
-
-        right.getChildren().addAll(sep1, new ListView<>());
-
-        VBox bottom=new VBox();
-        bottom.setMinHeight(15);
-        bottom.getChildren().addAll(new Separator(Orientation.HORIZONTAL));
-
-        ContextMenu c=new ContextMenu();
-        MenuItem add=new MenuItem("Add Entity");
-        add.setOnAction(event -> {
-            Stage st=new Stage();
-            Pane lay=new Pane();
-            Scene sc=new Scene(lay, 400, 400);
-            st.setScene(sc);
-            st.setTitle("ALL PARAMETERS HERE");
-            st.show();
-        });
-        MenuItem rem=new MenuItem("Remove Entity");
-        rem.setDisable(true);
-        c.getItems().addAll(add, rem);
-
-        /*
-            STARTING VALUES BEGIN
-        */
-        Random rand = new Random();
-
-        PlanetarySystem PS = new PlanetarySystem();
-
-        double SCALE = 1e-9; // around 1 Mercury RADIUS per default screen width
-        double timePeroid = 864; // 0.01 day
-        double MILLIS = 1;
-        //    STARTING VALUES END
-
-        center.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-            PS.AddPlanet(new Planet(event.getX()/SCALE, event.getY()/SCALE, 0, 0,0,0,Planet.NAME.OurSun ));
-        });
-
-        center.setOnContextMenuRequested(event -> {
-            x=event.getX();
-            y=event.getY();
-            c.show(center, event.getScreenX(), event.getScreenY());
-        });
-
+        layout.setCenter(mainBox);
         layout.setTop(menuBar);
-        layout.setRight(right);
-        layout.setCenter(center);
-        layout.setBottom(bottom);
+        layout.setRight(rightBox);
+        layout.setBottom(bottomBox);
 
         stage.setScene(scene);
         stage.show();
-
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(MILLIS),
-            new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent t) {
-
-                    PS.Simulate(timePeroid);
-                    while(PS.ConnectCollidedPlanets());
-                    center.getChildren().clear();
-
-                    for(int i=0; i<PS.planets.size(); i++)
-                    {
-                        //Circle ball = new Circle(10, Color.color(0,0,0));
-                        Circle ball = new Circle(PS.planets.get(i).radius * SCALE*10, PS.planets.get(i).c);
-                        //Circle ball = new Circle(10, PS.planets.get(i).c);
-
-                        ball.relocate(PS.planets.get(i).x_pos * SCALE, PS.planets.get(i).y_pos * SCALE);
-                        center.getChildren().add(ball);
-                        //System.out.println(PS.planets.size());
-                    }
-                }
-            }));
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
     }
 }
---
+++ planetary-simulator/src/main/simulator/planetary/PlanetaryGUI.java
@@ -0,0 +1,136 @@
+package main.simulator.planetary;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import main.simulator.planetary.PlanetaryExceptions.MenuException;
+
+import java.util.Random;
+
+public class PlanetaryGUI {
+
+   // PlanetaryMenuBar menuBar;
+
+    //Zwraca objekt typu MenuBar, który jest wyposażony w opcje
+    public static PlanetaryMenuBar makePlanetaryBar() throws MenuException {
+        PlanetaryMenuBar menuBar=new PlanetaryMenuBar();
+        menuBar.addMenu("File");
+        menuBar.addMenuItem("File", "Save...", null);
+        menuBar.addMenuItem("File", "Save as...", null);
+        menuBar.addSeparator("File");
+        menuBar.addMenuItem("File", "Exit", BasicEvents.closeEvent);
+        menuBar.addMenu("Edit");
+        menuBar.addMenuItem("Edit", "Undo", null);
+        menuBar.addMenuItem("Edit", "Redo", null);
+        menuBar.addSeparator("Edit");
+        menuBar.addMenuItem("Edit", "Cut", null);
+        menuBar.addMenuItem("Edit", "Copy", null);
+        menuBar.addMenuItem("Edit", "Paste", null);
+        menuBar.addMenu("Help");
+        menuBar.addMenuItem("Help", "About", null);
+
+        return menuBar;
+    }
+
+    public static HBox makeRightBox() {
+        HBox box=new HBox();
+        box.setMinWidth(250);
+        ListView<String> listView=new ListView<>();
+
+        box.getChildren().addAll(listView);
+
+        return box;
+    }
+
+    public static VBox makeBottomBox() {
+        VBox box=new VBox();
+        box.setMinHeight(15);
+
+        box.getChildren().addAll();
+
+        return box;
+    }
+
+    public static ContextMenu makeContextMenu() {
+        ContextMenu contextMenu=new ContextMenu();
+        MenuItem add=new MenuItem("Add Entity...");
+        MenuItem remove=new MenuItem("Remove Entity");
+        remove.setDisable(true);
+
+        contextMenu.getItems().addAll(add, remove);
+
+        return contextMenu;
+    }
+
+    public static EventHandler<ContextMenuEvent> onContextMenuMainBoxEventHandler() {
+        ContextMenu contextMenu=makeContextMenu();
+        return e->{
+            contextMenu.show(Main.mainBox, e.getScreenX(), e.getScreenY());
+        };
+    }
+
+    public static Pane makeMainBox() {
+        Pane center=new Pane();
+
+        /*
+            STARTING VALUES BEGIN
+        */
+        Random rand = new Random();
+
+        PlanetarySystem PS = new PlanetarySystem();
+
+        double SCALE = 1e-9; // around 1 Mercury RADIUS per default screen width
+        double timePeroid = 864; // 0.01 day
+        double MILLIS = 1;
+        //    STARTING VALUES END
+
+        center.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+            PS.AddPlanet(new Planet(event.getX()/SCALE, event.getY()/SCALE, 0, 0,0,0,Planet.NAME.OurSun ));
+        });
+        ContextMenu c=PlanetaryGUI.makeContextMenu();
+
+        EventHandler<ContextMenuEvent> contextMenuEvent=PlanetaryGUI.onContextMenuMainBoxEventHandler();
+        center.setOnContextMenuRequested(contextMenuEvent);
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(MILLIS),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent t) {
+
+                        PS.Simulate(timePeroid);
+                        while(PS.ConnectCollidedPlanets());
+                        center.getChildren().clear();
+
+                        for(int i=0; i<PS.planets.size(); i++)
+                        {
+                            //Circle ball = new Circle(10, Color.color(0,0,0));
+                            Circle ball = new Circle(PS.planets.get(i).radius * SCALE*10, PS.planets.get(i).c);
+                            //Circle ball = new Circle(10, PS.planets.get(i).c);
+
+                            ball.relocate(PS.planets.get(i).x_pos * SCALE, PS.planets.get(i).y_pos * SCALE);
+                            center.getChildren().add(ball);
+                            //System.out.println(PS.planets.size());
+                        }
+                    }
+                }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+
+        return center;
+    }
+}
